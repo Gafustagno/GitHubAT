4346070c89e6a184007f678ddf11ba866429ee13
Gabriel Sá Fustagno <127206447+Gafustagno@users.noreply.github.com>
2024-10-02 15:59:24 +0000

    VersaoFinal

--- src/Arquitetura.jsx
+++ src/Arquitetura.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import nvidiaXamd from "./nvidiaXamd.jpeg";
 
 const Arquitetura = () => {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        const newProgress = prevProgress + 1;
+        if (newProgress >= 100) {
+          clearInterval(interval);
+          setLoading(false);
+        }
+        return newProgress;
+      });
+    }, 100);
+  }, []);
+
   const styles = {
     container: {
       display: "flex",
@@ -23,29 +40,56 @@ const Arquitetura = () => {
       height: "50vh",
       marginBottom: "20px",
     },
+    imageContainer: {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      display: loading ? "block" : "none",
+    },
+    image: {
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+    },
+    progressBar: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: `${progress}%`,
+      height: "20px",
+      backgroundImage:
+        "linear-gradient(to right, #000000 25%, #2cc22c 50%, #8B0000 75%, #ffffff 100%)",
+    },
   };
 
   return (
     <div className="containerArquitetura">
-      <div style={styles.container}>
-        <h1 style={styles.title}>Nvidia</h1>
-        <iframe
-          style={styles.video}
-          src="https://www.youtube.com/embed/fj245xMr-BM"
-          title="Video Nvidia"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
+      {loading ? (
+        <div style={styles.imageContainer}>
+          <img src={nvidiaXamd} alt="Loading" style={styles.image} />
+          <div style={styles.progressBar}></div>
+        </div>
+      ) : (
+        <div style={styles.container}>
+          <h1 style={styles.title}>Nvidia</h1>
+          <iframe
+            style={styles.video}
+            src="https://www.youtube.com/embed/fj245xMr-BM"
+            title="Video Nvidia"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
 
-        <h1 style={styles.title}>AMD Radeon</h1>
-        <iframe
-          style={styles.video}
-          src="https://www.youtube.com/embed/XAGFS8d_qLw"
-          title="Video AMD Radeon"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
+          <h1 style={styles.title}>AMD Radeon</h1>
+          <iframe
+            style={styles.video}
+            src="https://www.youtube.com/embed/XAGFS8d_qLw"
+            title="Video AMD Radeon"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 };
